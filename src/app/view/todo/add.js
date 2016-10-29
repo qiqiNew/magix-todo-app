4d0b5644210c8c1ab785170a62a9176c96540222
deqing.qdq <deqing.qdq@alibaba-inc.com>
2016-10-29 15:22:55 +0800

    完成事件处理

--- src/app/view/todo/add.js
+++ src/app/view/todo/add.js
@@ -1,9 +1,37 @@
-define('app/view/todo/add',['magix'],function(Magix ){
+define('app/view/todo/add',['magix','jquery'],function(Magix ,$ ){
+
 
 return Magix.View.extend({
-    tmpl: "<div>todo add</div>",
+    tmpl: "<div><h2>新建Todo</h2><form mx-submit=\"saveTodo()\"><div class=\"form-group\"><label>Name:</label><input type=\"text\" class=\"form-control\" name=\"name\"></div><div class=\"form-group\"><button type=\"submit\" class=\"btn btn-default\">提交</button></div></form></div>",
     render: function() {
         this.setHTML(this.id, this.tmpl)
+    },
+
+    /**
+     * 事件监听器方括号内注明方法所监听的事件, 事件监听器的this为view
+     * @param e {Event} 所使用框架的event事件, 如果是jquery, 那就是jquery事件, kissy类似
+     **/
+    'saveTodo<submit>': function (e) {
+        e.preventDefault()
+
+        // 通过id查找模块容器DOM节点
+        var $main = $('#' + this.id)
+        var $name = $main.find('[name=name]')
+
+        $.ajax({
+            url: '/api/todo/add.json',
+            data: {
+                name: $name.val()
+            }
+        }).then(function (resp) {
+            if (resp.info.ok) {
+                alert('保存成功')
+                Magix.Router.to('/todo/list')
+            } else {
+                alert(resp.info.message)
+            }
+        })
+
     }
 })
 
--- tmpl/app/view/todo/add.js
+++ tmpl/app/view/todo/add.js
@@ -1,8 +1,36 @@
 var Magix = require('magix')
+var $ = require('jquery')
 
 module.exports = Magix.View.extend({
     tmpl: '@add.html',
     render: function() {
         this.setHTML(this.id, this.tmpl)
+    },
+
+    /**
+     * 事件监听器方括号内注明方法所监听的事件, 事件监听器的this为view
+     * @param e {Event} 所使用框架的event事件, 如果是jquery, 那就是jquery事件, kissy类似
+     **/
+    'saveTodo<submit>': function (e) {
+        e.preventDefault()
+
+        // 通过id查找模块容器DOM节点
+        var $main = $('#' + this.id)
+        var $name = $main.find('[name=name]')
+
+        $.ajax({
+            url: '/api/todo/add.json',
+            data: {
+                name: $name.val()
+            }
+        }).then(function (resp) {
+            if (resp.info.ok) {
+                alert('保存成功')
+                Magix.Router.to('/todo/list')
+            } else {
+                alert(resp.info.message)
+            }
+        })
+
     }
 })
